f833b4249c6fd62067c295132ea20b26873413a4
Manish Bhandari <maniishbhandarii@gmail.com>
2024-03-05 20:48:13 +0530

    utlis code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port no. ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED WITH ERROR: ", err);
+  });
 
 /*
 import express from "express";
