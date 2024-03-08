dd3f479f7aa82719e87a30ac0e0fd7f5b69e67c0
Manish Bhandari <maniishbhandarii@gmail.com>
2024-03-08 16:52:29 +0530

    added video models and user models

--- src/utlis/apiError.js
+++ src/utlis/apiError.js
@@ -1,24 +1,23 @@
-class ApiError extends Error{
-    constructor(
-        statusCode,
-        message= "Something went wrong",
-        errors = [],
-        stack = ""
-    ){
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.errors = errors;
-        this.message = message;
-        this.success = false;
-        
-        if(stack){
-            this.stack = stack;
-        }
-        else{
-            Error.captureStackTrace(this, this.constructor)
-        }
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.errors = errors;
+    this.message = message;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-export {ApiError}
+export { ApiError };
